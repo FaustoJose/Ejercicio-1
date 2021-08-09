0b9c5e2d01c87f6fbaae906a013482bfd0a841d3
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:03:35 -0400

    Borrar pantalla

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BorrarPantalla();
+            
+        }
+        public static void BorrarPantalla()
+        {
+             Console.Clear();
+
+            int i;
+            for (i = 0; i <= (25-1); i++)
+            {
+                Console.WriteLine("____________________________________________________________________________________");
+            }
+        }
+
+    }
+    
+}
